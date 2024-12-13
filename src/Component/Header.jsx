22d9e7c69dcb49413520e718ec443a94905b69e8
Tony Oloriogun <webdevloper186@gmail.com>
2024-12-13 21:37:38 +0100

    updated the header

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -3,47 +3,16 @@ import { useState } from "react";
 import cv from "../assets/Oloriogun_Tony _Frontend_Developer_Resume.pdf"
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
   return (
     <header>
-      <div className=" border-b-2 border-White bg-Mid-Blue flex justify-between items-center h-20 font-bold text-White lg:text-Violet px-20">
-        <div className="flex justify-between">
-          <h1 className="text-xl pl-10 lg:pl-20 lg:text-3xl">
+      <div className=" border-b-2 px-10  border-White bg-Mid-Blue flex justify-between items-center h-20 font-bold text-White lg:text-Violet lg:px-40">
+          <h1 className="text-xl lg:text-3xl">
             Dev <span className="text-Lemon">Tony</span>{" "}
           </h1>
-        </div>
-
-        <div
-          className={
-            open
-              ? "absolute h-screen lg:h-20 w-full px-10 top-20 text-lg bg-White text-Mid-Blue   block text-center "
-              : "hidden lg:flex"
-          }
-        >
-          <div className="flex flex-col items-center mt-40 lg:mt-0 list-none lg:ml-10 lg:flex-row lg:text-xl ">
-            <li className="mx-4 py-5 lg:py-0">
-              <a href="">About</a>
-            </li>
-            <li className="mx-4 pb-5 lg:pb-0">
-              <a href="">Contact</a>
-            </li>
-            <li className="mx-4 pb-5 lg:pr-20 lg:pb-0">
-              <a download={cv} href={cv}>Resume</a>
-            </li>
-          </div>
-        </div>
-
-        {open ? (
-          <FaTimes
-            onClick={() => setOpen(!open)}
-            className="text-Violet text-3xl mr-10 lg:hidden"
-          />
-        ) : (
-          <FaBars
-            onClick={() => setOpen(!open)}
-            className="text-Violet text-3xl mr-10 lg:hidden"
-          />
-        )}
+          <li className="list-none lg:text-xl">
+            <a download={cv} href={cv}>Resume</a>
+          </li>
       </div>
     </header>
   );
---
+++ src/Component/Unused.jsx
@@ -0,0 +1,47 @@
+{/* <div className=" border-b-2 border-White bg-Mid-Blue flex justify-between items-center h-20 font-bold text-White lg:text-Violet lg:px-20">
+        <div className="flex justify-between items-center">
+          <h1 className="text-xl pl-10 lg:pl-20 lg:text-3xl">
+            Dev <span className="text-Lemon">Tony</span>{" "}
+          </h1>
+          <li className="mx-4 list-none">
+            <a download={cv} href={cv}>Resume</a>
+          </li>
+        </div> */}
+
+        <div
+          // className={
+          //   open
+          //     ? "absolute h-screen lg:h-20 w-full px-10 top-20 text-lg bg-White text-Mid-Blue   block text-center "
+          //     : "hidden lg:flex"
+          // }
+          // className={
+          //   open
+          //     ? "absolute h-screen lg:h-20 w-full px-10 top-20 text-lg bg-White text-Mid-Blue   block text-center "
+          //     : "hidden lg:flex"
+          // }
+        >
+          {/* <div className="flex flex-col items-center mt-40 lg:mt-0 list-none lg:ml-10 lg:flex-row lg:text-xl "> */}
+            {/* <li className="mx-4 py-5 lg:py-0">
+              <a href="">About</a>
+            </li>
+            <li className="mx-4 pb-5 lg:pb-0">
+              <a href="">Contact</a>
+            </li> */}
+            {/* <li className="mx-4 pb-5 lg:pr-20 lg:pb-0">
+              <a download={cv} href={cv}>Resume</a>
+            </li> */}
+          </div>
+        // </div>
+
+        {/* {open ? (
+          <FaTimes
+            onClick={() => setOpen(!open)}
+            className="text-Violet text-3xl mr-10 lg:hidden"
+          />
+        ) : (
+          <FaBars
+            onClick={() => setOpen(!open)}
+            className="text-Violet text-3xl mr-10 lg:hidden"
+          />
+        )} */}
+    //   </div>
